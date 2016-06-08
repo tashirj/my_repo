280926014dcf5b38f82f7ea4b77edaffbca9b616
tushar <tushdudhade@gmail.com>
2016-06-08 16:41:16 +0530

    index file added

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,14 @@
 # Copyright 2016, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-package 'httpd'	 do
-	action: install
+package 'apache2'	 do
+	action:install
+end
+service 'apache2' do
+	action [ :enable, :start ] 
+	
+end
+cookbook_file	'/var/www/html'	do
+	source 'index.html'
+	mode 0644
 end
